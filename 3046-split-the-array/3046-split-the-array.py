73d1d32b970b47b7a036c364146953aba52ffbc9
Kunal Chaudhari <92564076+kc-codes@users.noreply.github.com>
2024-09-28 00:06:00 +0530

    Time: 37 ms (95.94%), Space: 16.5 MB (73.29%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.py
+++ 3046-split-the-array/3046-split-the-array.py
@@ -1,13 +1,33 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
        
-        freq = Counter(nums)
         
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] = d[i] +1
+            else:
+                d[i] = 1 
+
         
-        for count in freq.values():
+        # freq = Counter(nums)
+        
+        for count in d.values():
             if count > 2:
                 return False
         
         
-        unique_elements = len(freq)
+        unique_elements = len(d)
         return unique_elements >= len(nums) // 2
+
+
+
+        # freq = Counter(nums)
+        
+        # for count in freq.values():
+        #     if count > 2:
+        #         return False
+        
+        
+        # unique_elements = len(freq)
+        # return unique_elements >= len(nums) // 2
